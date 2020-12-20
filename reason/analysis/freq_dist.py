36a28d8582f8edd8f2b22617bf669c3ece0778a6
alisoltanirad <soltaniradali@gmail.com>
2020-12-20 15:06:26 +0330

    Update freqdist class

--- reason/analysis/freq_dist.py
+++ reason/analysis/freq_dist.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from copy import deepcopy
 
 from reason.tokenize import word_tokenize
 
@@ -12,6 +13,17 @@ class FreqDist:
     def __str__(self):
         return str(self._counter)
 
+    def __getitem__(self, key):
+        return self._counter[key]
+
+    def __setitem__(self, key, value):
+        self._counter[key] = value
+
+    def __delitem__(self, key):
+        del self._counter[key]
+
     def most_common(self, n=1):
         return self._counter.most_common(n)
 
+    def copy(self):
+        return deepcopy(self)

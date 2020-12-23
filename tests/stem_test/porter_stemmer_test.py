78cfbd5eb49a95c449b0e70487e291977d2d5539
alisoltanirad <soltaniradali@gmail.com>
2020-12-23 17:37:38 +0330

    Update porter stemmer tests

--- tests/stem_test/porter_stemmer_test.py
+++ tests/stem_test/porter_stemmer_test.py
@@ -13,3 +13,41 @@ def test_step1a_3():
 def test_step1a_4():
     assert porter_stem('cats') == 'cat'
 
+def test_step1b_1():
+    assert porter_stem('feed') == 'feed'
+
+def test_step1b_2():
+    assert porter_stem('agreed') == 'agree'
+
+def test_step1b_1():
+    assert porter_stem('bled') == 'bled'
+
+def test_step1b_1():
+    assert porter_stem('plastered') == 'plaster'
+
+def test_step1b_1():
+    assert porter_stem('sing') == 'sing'
+
+def test_step1b_1():
+    assert porter_stem('motoring') == 'motor'
+
+def test_step1b_1():
+    assert porter_stem('conflated') == 'conflate'
+
+def test_step1b_1():
+    assert porter_stem('troubled') == 'trouble'
+
+def test_step1b_1():
+    assert porter_stem('sized') == 'size'
+
+def test_step1b_1():
+    assert porter_stem('tanned') == 'tan'
+
+def test_step1b_1():
+    assert porter_stem('falling') == 'fall'
+
+def test_step1b_1():
+    assert porter_stem('failing') == 'fail'
+
+def test_step1b_1():
+    assert porter_stem('filing') == 'file'

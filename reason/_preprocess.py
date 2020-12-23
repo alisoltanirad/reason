f7e0b2e5f577b90eed4a3c6096ff378ce7d593fc
alisoltanirad <soltaniradali@gmail.com>
2020-12-24 00:40:41 +0330

    Update preprocess module

--- reason/_preprocess.py
+++ reason/_preprocess.py
@@ -1,6 +1,52 @@
-from .tokenize import sent_tokenize
+# -*- coding: utf-8 -*-
+"""Preprocess module.
+
+API:
+* *preprocess* (function): Easy-to-use preprocessing tool.
+
+Example:
+    Preprocess an string:
+
+        >>> from reason import preprocess
+
+        >>> text = "What's up? I love using Reason library!"
+        >>> preprocess(text)
+        [["what's", 'up', '?'], ['i', 'love', 'us', 'reason', 'librari', '!']]
+
+"""
+from .tokenize import sent_tokenize, word_tokenize
+from .stem import PorterStemmer
 
 
-def preprocess(text):
-    tokens = sent_tokenize(text)
-    return tokens
+def preprocess(corpus):
+    """Stem finding method.
+
+    Tokenize input corpus to sentences and words, then finds token stems
+    using porter stemmer.
+
+    Args:
+        corpus (str or list of str): Text.
+
+    Returns:
+        list: Preprocessed text.
+
+    Raises:
+        Exception: If input is not string or a list of strings.
+
+    """
+    try:
+        sents = sent_tokenize(corpus)
+    except TypeError:
+        raise TypeError(
+            'Preprocess input must be string or a list of strings.'
+        )
+
+    stemmer = PorterStemmer()
+    output = list()
+    for sent in sents:
+        stems = list()
+        for token in word_tokenize(sent):
+            stems += stemmer.stem(token)
+        output.append(stems)
+
+    return output

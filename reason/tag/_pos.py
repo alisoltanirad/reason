c02788338e91dfe5f7809191f58109b9597b5d65
alisoltanirad <soltaniradali@gmail.com>
2020-12-24 19:46:15 +0330

    Add pos tagger reason/tag/__init__.py

--- reason/tag/_pos.py
+++ reason/tag/_pos.py
@@ -1,1 +1,33 @@
-class POSTagger:
+from ._tagger import BaseTagger
+from ._default import DefaultTagger
+from ._regex import RegexTagger
+from ._lookup import LookupTagger
+from ._pos_tagger_data import get_re_collection, get_freq_100
+
+
+class POSTagger(BaseTagger):
+    """POS tagger
+
+    Part of speech tagger. Uses lookup, regex and default taggers for tagging.
+
+    """
+
+    def __init__(self, backoff=None):
+        """RegexTagger Constructor.
+
+        Args:
+            backoff (tagger, optional): Backoff tagger object.
+
+        """
+        super().__init__(backoff)
+
+        default_tagger = DefaultTagger('NN')
+        re_collection = get_re_collection()
+        regex_tagger = RegexTagger(re_collection, backoff=default_tagger)
+        lookup_data = get_freq_100()
+        lookup_tagger = LookupTagger(lookup_data, backoff=regex_tagger)
+        self._tagger = lookup_tagger
+
+
+    def _token_tag(self, token):
+        return self._tagger.tag(token)[0][1]

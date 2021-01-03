712509d91458eb9597f0e484ef9db6d9bb31a50f
alisoltanirad <soltaniradali@gmail.com>
2021-01-04 00:54:55 +0330

    Update elbow method

--- reason/cluster/_elbow_method.py
+++ reason/cluster/_elbow_method.py
@@ -1,14 +1,34 @@
-import numpy as np
+def elbow_method(data, clusterer, max_k):
+    """Elbow method function.
+
+    Takes dataset, "Reason" clusterer class and returns the optimum number of
+    clusters in range of 1 to max_k.
+
+    Args:
+        data (pandas.DataFrame or list of dict): Data to cluster.
+        clusterer (class): "Reason" clusterer.
+        max_k (int): Maximum k to analysis.
+
+    Returns:
+        k (int): Optimum number of clusters.
+
+    """
+    k = None
+    score = _get_score(data, clusterer, 1)
+    for i in range(2, max_k + 1):
+        current_score = _get_score(data, clusterer, i)
+        if (score - current_score) < (score / 8):
+            k = i - 1
+            break
+        score = current_score
+
+    if k == None:
+        return max_k
+
+    return k
 
 
-def elbow_method(data, clusterer, max_k, verbose=1):
-    k_range = range(1, max_k + 1)
-    scores = []
-    for i in k_range:
-        if verbose == 1:
-            print('k = {}:'.format(i))
-        model = clusterer()
-        model.fit(data, k=i, verbose=verbose)
-        scores.append(model.inertia())
-
-    return np.array(scores)
+def _get_score(data, clusterer, k):
+    model = clusterer()
+    model.fit(data, k=k, verbose=0)
+    return model.inertia()

0096896c89a04c3e1d32b395d74a867d2c450d92
alisoltanirad <soltaniradali@gmail.com>
2021-02-08 14:10:22 -0500

    Add KNN classifier class

--- reason/classify/__init__.py
+++ reason/classify/__init__.py
@@ -6,7 +6,9 @@ Classification tools using machine learning methods.
 API:
 
 * NaiveBayesClassifier (class): Classifier which uses naive bayes algorithm.
+* KNNClassifier (class): Classifier which uses KNN algorithm.
 
 """
 
 from ._naivebayes import NaiveBayesClassifier
+from ._knn import KNNClassifier

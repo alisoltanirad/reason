b31f70ac3f86437f08b24cb817bb7098462069d7
alisoltanirad <soltaniradali@gmail.com>
2023-01-24 20:01:54 -0500

    Update errors for not implemented classes

--- reason/classify/__init__.py
+++ reason/classify/__init__.py
@@ -6,11 +6,9 @@ Classification tools using machine learning methods.
 API:
 
 * NaiveBayesClassifier (class): Classifier which uses naive bayes algorithm.
-* KNNClassifier (class): Classifier which uses KNN algorithm.
-* DecisionTreeClassifier (class): Classifier which uses decision tree algorithm.
+* [To-Do] KNNClassifier (class): Classifier which uses KNN algorithm.
+* [To-Do] DecisionTreeClassifier (class): Classifier which uses decision tree algorithm.
 
 """
 
-from ._decisiontree import DecisionTreeClassifier
-from ._knn import KNNClassifier
 from ._naivebayes import NaiveBayesClassifier

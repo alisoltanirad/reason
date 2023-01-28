22112b5b2edd6285d9c45845df04835603035c61
alisoltanirad <soltaniradali@gmail.com>
2023-01-27 19:40:40 -0500

    Update nox sessions

--- noxfile.py
+++ noxfile.py
@@ -1,18 +1,26 @@
 import nox
 
-@nox.session
+LINT_FILES = ["reason", "tests", "noxfile.py", "setup.py"]
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def test(session):
+    session.install("-r", "requirements.txt")
+    session.install("pytest")
     session.run("pytest", "-v", "tests")
+
 
 @nox.session
 def isort(session):
-    session.install('isort')
-    session.run('isort', 'reason', 'tests')
+    session.install("isort")
+    session.run("isort", *LINT_FILES)
+
 
 @nox.session
 def black(session):
-    session.install('black')
-    session.run('black', 'reason', 'tests')
+    session.install("black")
+    session.run("black", *LINT_FILES)
+
 
 @nox.session
 def lint(session):

0538435f82d632eea0ff306e77e1ac28a7fbd5b4
alisoltanirad <soltaniradali@gmail.com>
2023-02-16 20:28:52 -0500

    Update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="reason",
-    version="1.0.1",
+    version="1.0.2",
     description="Natural language processing toolbox",
     long_description=long_description,
     long_description_content_type="text/markdown",
